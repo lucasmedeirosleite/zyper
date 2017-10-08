641f0330a09739511a662e935716e36451045f26
Lucas Medeiros <lucastoc@gmail.com>
2017-10-08 00:46:14 -0300

    linter for rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -24,8 +24,8 @@ VCR.configure do |config|
 end
 
 RSpec.configure do |config|
-  %i[controller view request].each do |type|
-    config.include ::Rails::Controller::Testing::TestProcess, type: type
+  %i(controller view request).each do |type|
+    config.incilude ::Rails::Controller::Testing::TestProcess, type: type
     config.include ::Rails::Controller::Testing::TemplateAssertions, type: type
     config.include ::Rails::Controller::Testing::Integration, type: type
   end
